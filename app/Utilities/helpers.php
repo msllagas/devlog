2aa413ed1432f66d6932163a2cf5c909568b9298
msllagas <mndyllagas@gmail.com>
2025-09-07 12:22:23 +0800

    feat: add env helper function

--- app/Utilities/helpers.php
+++ app/Utilities/helpers.php
@@ -11,3 +11,25 @@ if (! function_exists('dd')) {
     }
 }
 
+if (!function_exists('env')) {
+    /**
+     * Get an environment variable.
+     *
+     * @param string $key
+     * @param mixed $default
+     * @return mixed
+     */
+    function env(string $key, $default = null): mixed
+    {
+        if (array_key_exists($key, $_ENV)) {
+            return $_ENV[$key];
+        }
+
+        if (array_key_exists($key, $_SERVER)) {
+            return $_SERVER[$key];
+        }
+
+        $value = getenv($key);
+        return $value !== false ? $value : $default;
+    }
+}
