335c19cc53afeba6aafa5aae3977fcdf041c6148
msllagas <mndyllagas@gmail.com>
2025-09-22 20:51:09 +0800

    feat: show posts on index

--- views/index.view.php
+++ views/index.view.php
@@ -3,10 +3,33 @@
 <?php require 'partials/banner.php'; ?>
 
 <main>
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1>Welcome To DevLog</h1>
+    <div class="mx-auto max-w-7xl px-2 py-6 sm:px-6 lg:px-8">
+        <div class="grid grid-cols-1 lg:grid-cols-2 gap-2 lg:p-6">
+            <?php foreach ($posts as $post): ?>
+                <a href="<?= htmlspecialchars($post['url']) ?>"
+                      class="flex items-start gap-4 w-full p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <!-- Avatar -->
+                    <div class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 text-white flex items-center justify-center font-bold uppercase">
+                        <?= htmlspecialchars(substr($post['author_name'], 0, 2)) ?>
+                    </div>
 
-        <a href="/post">Go to Post</a>
+                    <div>
+                        <div class="mb-4">
+                            <p class="!text-base font-normalfont-semibold text-gray-500 dark:text-gray-200">
+                                <?= htmlspecialchars($post['author_name']) ?>
+                            </p>
+                            <p class="text-sm font-normal italic text-gray-500 dark:text-gray-200">
+                                <?= date("M. j, Y", strtotime($post['published_at'])) ?>
+                            </p>
+                        </div>
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            <?= htmlspecialchars($post['title']) ?>
+                        </h5>
+                    </div>
+                </a>
+
+            <?php endforeach; ?>
+        </div>
     </div>
 </main>
 
